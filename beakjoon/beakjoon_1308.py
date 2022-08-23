4850a858723820a7426f21f3c761d007deb8e3c5
정혁기 <sawol22@jeonghyeoggiui-MacBookPro.local>
2022-08-23 10:50:26 +0900

    22.08.23

--- beakjoon/beakjoon_1308.py
+++ beakjoon/beakjoon_1308.py
@@ -1,16 +1,20 @@
-# y1, m1, d1 = map(int, input().split())
-# y2, m2, d2 = map(int, input().split())
-y1, m1, d1 = [2008, 1, 2]
-y2, m2, d2 = [3008, 1, 1]
+y1, m1, d1 = map(int, input().split())
+y2, m2, d2 = map(int, input().split())
         
 import datetime as dt
 d_day = str(dt.date(y2, m2, d2) - dt.date(y1, m1, d1)).split()[0]
 
-if y2-y1 == 1000:
-    if m2 > m1: print('gg')
+gg = False
+if y2-y1 > 1000:
+    gg = True
+elif y2-y1 == 1000:
+    if m2 > m1:
+        gg = True
     elif m2 == m1:
-        if d2 >= d1: print('gg')
-elif y2-y1 > 1000:
-    print('gg')
-else:
+        if d2 >= d1:
+            gg = True
+            
+if not gg:
     print(f'D-{d_day}')
+else:
+    print('gg')
