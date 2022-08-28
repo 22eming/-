75bdb0ba898d744fb0c63e4e8b9340989fcdc34c
정혁기 <sawol22@jeonghyeoggiui-MacBookPro.local>
2022-08-29 00:09:44 +0900

    22.08.29

---
+++ beakjoon/beakjoon_1316.py
@@ -0,0 +1,16 @@
+n = int(input())
+answer = 0
+for _ in range(n):
+    group_word = list(input())
+    visited = set()
+    temp = ''
+    for g_w in group_word:
+        if g_w in visited and g_w != temp:
+            break
+        visited.add(g_w)
+        temp = g_w
+    else:
+        answer += 1
+
+print(answer)
+    
