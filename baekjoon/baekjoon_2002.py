55a7d8fb5b19d2ce16272ff483b286157db542cc
22eming <gurrl96@gmail.com>
2022-10-13 17:32:34 +0900

    22.10.13

--- baekjoon/baekjoon_2002.py
+++ baekjoon/baekjoon_2002.py
@@ -1,3 +1,13 @@
 N = int(input())
 input_car = {input(): i for i in range(N)}
-output_car = {input(): i for i in range(N)}
+visited = [False]*N
+answer = 0
+
+for _ in range(N):
+    out_car = input()
+    i = input_car[out_car]
+    sum_i = sum(visited[:i])
+    if sum_i < i:
+        answer += 1
+    visited[i] = True
+print(answer)
