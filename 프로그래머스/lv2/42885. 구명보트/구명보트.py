b8ee295b37fa7332383094bab4e71913c7f556e2
sawol <77485227+22eming@users.noreply.github.com>
2022-10-16 21:48:22 +0900

    [level 2] Title: 구명보트, Time: 8.96 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,10 @@
+def solution(people, limit):
+    people.sort()
+    answer = 0
+    start,end = 0, len(people)-1
+    while start <= end:
+        answer += 1
+        if people[start] + people[end] <= limit:
+            start += 1
+        end -= 1
+    return answer
