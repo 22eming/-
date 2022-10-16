47649fa2d57b1d2a46d10ee213a7ff66a2b5ec24
sawol <77485227+22eming@users.noreply.github.com>
2022-10-16 21:38:29 +0900

    [level 2] Title: 후보키, Time: 0.46 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -0,0 +1,24 @@
+from itertools import combinations
+
+
+def solution(relation):
+    answer = []
+    lenth = len(relation[0])
+    relations = [i for i in range(lenth)]
+    for i in range(1, lenth + 1):
+        for j in combinations(relations, i):
+            # 최소성 검사
+            if [1 for m in answer if set(m).issubset(set(j))]:
+                continue
+
+            s = []
+            # 모든 행 검사
+            for k in range(len(relation)):
+                # 특정 열 추출
+                rel = [relation[k][l] for l in j]
+                # 유일성 판별
+                if rel not in s:
+                    s.append(rel)
+            if len(s) == len(relation):
+                answer.append(j)
+    return len(answer)
