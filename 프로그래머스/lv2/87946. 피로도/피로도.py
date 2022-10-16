89d45cba944614213470054522a9ca4c69dfb3e6
sawol <77485227+22eming@users.noreply.github.com>
2022-10-16 21:43:45 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 9.91 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+
+def solution(k, dungeons):
+    answer = -1
+    for dungeon in permutations([i for i in range(len(dungeons))]):
+        fatigue = k
+        cnt = 0
+        for i in dungeon:
+            if dungeons[i][0] <= fatigue:  # 피로도를 충족
+                fatigue -= dungeons[i][1]
+                cnt += 1
+            else:
+                answer = max(answer, cnt)
+                break
+        else:
+            answer = max(answer, cnt)
+            
+    return answer
