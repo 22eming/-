231e1d76807f33c13c5c79c6e8e9dae5663fa2df
sawol <77485227+22eming@users.noreply.github.com>
2022-10-16 21:18:50 +0900

    [Bronze II] Title: 저항, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,3 @@
+R_dict = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+a, b, c = R_dict.index(input()), R_dict.index(input()), R_dict.index(input())
+print((a*10+b)*10**c)
