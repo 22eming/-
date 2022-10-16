ab16e18ac8f4ad380463ae46a8e951385547bd21
sawol <77485227+22eming@users.noreply.github.com>
2022-10-16 21:20:07 +0900

    [Silver V] Title: D-Day, Time: 92 ms, Memory: 35728 KB -BaekjoonHub

---
+++ 백준/Silver/1308. D－Day/D－Day.py
@@ -0,0 +1,21 @@
+y1, m1, d1 = map(int, input().split())
+y2, m2, d2 = map(int, input().split())
+
+        
+import datetime as dt
+d_day = str(dt.date(y2, m2, d2) - dt.date(y1, m1, d1)).split()[0]
+
+gg = False
+if y2-y1 > 1000:
+    gg = True
+elif y2-y1 == 1000:
+    if m2 > m1:
+        gg = True
+    elif m2 == m1:
+        if d2 >= d1:
+            gg = True
+            
+if not gg:
+    print(f'D-{d_day}')
+else:
+    print('gg')
