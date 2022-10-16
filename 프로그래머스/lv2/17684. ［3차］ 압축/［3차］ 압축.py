78f77980cba3543495cf9191075642e1f0aa7417
sawol <77485227+22eming@users.noreply.github.com>
2022-10-16 21:45:53 +0900

    [level 2] Title: [3차] 압축, Time: 0.55 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -0,0 +1,17 @@
+def solution(msg):
+    answer = []
+    book = {}
+    for idx,i in enumerate(range(65,91)):
+        book[chr(i)] = idx+1
+    i = 0
+    while i < len(msg):
+        j = 2
+        while msg[i:i+j] in book.keys() and i+j <= len(msg):
+            j += 1
+        else:
+            answer.append(book[msg[i:i+j-1]])
+            book[msg[i:i+j]] = len(book.values())+1
+            if j > 2:
+                i += j-2
+        i += 1
+    return answer
