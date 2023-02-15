764dbb6e82bed7da3cd43eb24daebeda92fd3589
sawol <77485227+22eming@users.noreply.github.com>
2023-02-16 00:18:32 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 300 ms, Memory: 51784 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,26 +1,23 @@
-from collections import defaultdict, deque
-N = int(input())
-tree = defaultdict(list)
-for i in range(N-1):
-    a, b = map(int, input().split())
-    tree[a].append(b)
-    tree[b].append(a)
+import sys
+from collections import deque
 
-root = set()
-answer = [0]*(N+1)
-stack = deque([])
+def bfs():
+    que = deque([1])
+    while que:
+        now =  que.popleft()            
+        for point in link[now]:
+            if not parent[point] and point != 1:
+                parent[point] = now
+                que.append(point)
+            
+N = int(sys.stdin.readline())
+link = [ [] for i in range(N+1)]
+parent = [0] * (N+1)
 
-def go_tree(q):
-    for c in tree[q]:
-        if c not in root:
-            answer[c] = q
-            root.add(c)
-            stack.append(c)
-
-stack.append(1)
-while stack:
-    q = stack.popleft()
-    go_tree(q)
-
-for a in answer[2:]:
-    print(a)
+for i in range(N - 1):
+    a, b = map(int, sys.stdin.readline().split())
+    link[a].append(b)
+    link[b].append(a)
+    
+bfs()
+print(*parent[2:], sep='\n')
